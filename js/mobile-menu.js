987dd40d8028f3a0431492f3c5ea2555ea3e6d5b
Dron010 <73987873+Dron010@users.noreply.github.com>
2021-06-27 00:02:55 +0300

    rev1.3
    fix-bag

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,12 +2,15 @@
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
     const mobileBtnClose = document.querySelector("[data-menu-close]");
+    const mobileBtnScrol = document.querySelector("[modal-2]");
   
     menuBtnRef.addEventListener("click", () => {
       mobileMenuRef.classList.toggle("is-open");
+      mobileBtnScrol. classList.toggle("no-scrol");
     });
   
     mobileBtnClose.addEventListener("click", () => {
       mobileMenuRef.classList.toggle("is-open");
+      mobileBtnScrol. classList.toggle("no-scrol");
     });
   })();
